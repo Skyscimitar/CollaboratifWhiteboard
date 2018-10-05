ce94309822e13526f2bd234d70f84a4b976ee6c9
schwobr <robin.schwob@student.ecp.fr>
2018-10-05 21:04:01 +0200

    ONT PEUT FAIRE DES DESSINS MAINTENANT

--- mainGUI/mainGUI/mainGUI/MainPage.xaml.cs
+++ mainGUI/mainGUI/mainGUI/MainPage.xaml.cs
@@ -4,14 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 
 namespace mainGUI
 {
     public partial class MainPage : ContentPage
     {
+
+        private Dictionary<long, SKPath> temporaryPaths = new Dictionary<long, SKPath>();
+        private List<SKPath> paths = new List<SKPath>();
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+            canvas.Clear(SKColors.White);
+
+            var touchPathStroke = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Black,
+                StrokeWidth = 5
+            };
+
+            // draw the paths
+            foreach (var touchPath in temporaryPaths)
+            {
+                canvas.DrawPath(touchPath.Value, touchPathStroke);
+            }
+            foreach (var touchPath in paths)
+            {
+                canvas.DrawPath(touchPath, touchPathStroke);
+            }
+
+        }
+
+        private void SKCanvasView_Touch(object sender, SKTouchEventArgs e)
+        {
+            switch (e.ActionType)
+            {
+                case SKTouchAction.Pressed:
+                    var p = new SKPath();
+                    p.MoveTo(e.Location);
+                    temporaryPaths[e.Id] = p;
+                    break;
+                case SKTouchAction.Moved:
+                    if (e.InContact)
+                        temporaryPaths[e.Id].LineTo(e.Location);
+                    break;
+                case SKTouchAction.Released:
+                    paths.Add(temporaryPaths[e.Id]);
+                    temporaryPaths.Remove(e.Id);
+                    break;
+                case SKTouchAction.Cancelled:
+                    temporaryPaths.Remove(e.Id);
+                    break;
+            }
+
+            e.Handled = true;
+            ((SKCanvasView)sender).InvalidateSurface();
         }
     }
 }
