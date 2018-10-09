5ef44beb4388b8c93fe39ad713f1394726e276f2
Daniel Taylor <danieltaylor@MacBook-Air-de-Admin.local>
2018-10-09 11:24:20 +0200

    yay! (and added mac UI)

--- mainGUI/mainGUI.Mac/AppDelegate.cs
+++ mainGUI/mainGUI.Mac/AppDelegate.cs
@@ -1,17 +1,32 @@
 ﻿using AppKit;
 using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
 
 namespace mainGUI.Mac
 {
     [Register("AppDelegate")]
-    public class AppDelegate : NSApplicationDelegate
+    public class AppDelegate : FormsApplicationDelegate
     {
+        NSWindow _window;
+
         public AppDelegate()
         {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "Xamarin.Forms on Mac!";
+            _window.TitleVisibility = NSWindowTitleVisibility.Hidden;
         }
+
+        public override NSWindow MainWindow { get { return _window; } }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
+            Forms.Init();
+            LoadApplication(new mainGUI.App());
+            base.DidFinishLaunching(notification);
             // Insert code here to initialize your application
         }
 
