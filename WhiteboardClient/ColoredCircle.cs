0dfd01fd360d40eaa1f3b26a608b96c9a0d49908
Daniel Taylor <danieltaylor@MacBook-Air-de-Admin.local>
2018-10-12 10:35:07 +0200

    fix circular dependency

---
+++ WhiteboardClient/ColoredCircle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SkiaSharp;
+
+namespace WhiteboardClient
+{
+    public class ColoredCircle
+    {
+        public SKColor Color { get; set; }
+        public SKPoint Center { get; set; }
+        public float Radius { get; set; }
+        public float StrokeWidth { get; set; }
+    }
+}
---
+++ WhiteboardClient/ColoredPath.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SkiaSharp;
+namespace WhiteboardClient
+{
+    public class ColoredPath
+    {
+        public SKColor Color { get; set; }
+        public SKPath Path { get; set; }
+        public float StrokeWidth { get; set; }
+    }
+}
