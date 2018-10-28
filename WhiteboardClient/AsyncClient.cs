64bf3427028404e2a2ced3e89acdca6953c0d039
Daniel Taylor <danny.taylor@live.fr>
2018-10-28 12:20:21 +0100

    added async client class

---
+++ WhiteboardClient/AsyncClient.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ColoredForms;
+
+namespace WhiteboardClient
+{
+    class AsyncClient
+    {
+
+        private const int port = 8080;
+        public  Socket Client { get; private set; }
+
+        private static ManualResetEvent connectDone = new ManualResetEvent(false);
+        private static ManualResetEvent sendDone = new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+
+        private IPAddress IpAddress;
+        private IPEndPoint RemoteEndPoint;
+        private PacketReceiver Receiver;
+
+
+        public AsyncClient(string IpAddress)
+        {
+            if (IPAddress.TryParse(IpAddress, out IPAddress address))
+            {
+                this.IpAddress = address;
+                RemoteEndPoint = new IPEndPoint(this.IpAddress, port);
+                Client = new Socket(this.IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            }
+            else
+            {
+                throw new ArgumentException(message: "invalid ip address provided");
+            }
+        }
+
+        public void StartClient()
+        {
+            Client.BeginConnect(RemoteEndPoint, new AsyncCallback(ConnectCallback), Client);
+            connectDone.WaitOne();
+            Receiver = new PacketReceiver(Client);
+            Thread receiveThread = new Thread(Receiver.StartReceiving);
+            receiveThread.Start();
+        }
+
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Client = (Socket)ar.AsyncState;
+                Client.EndConnect(ar);
+                Console.WriteLine("Socket connected to : {0}", Client.RemoteEndPoint.ToString());
+                connectDone.Set();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void SendData(JObject json)
+        {
+            string Json = json.ToString();
+            PacketSender sender = new PacketSender(Client);
+            sender.Send(Json);
+        }
+
+        
+        public void Send(ColoredCircle circle)
+        {
+            string colourHash = circle.Color.ToString();
+            float x = circle.Center.X;
+            float y = circle.Center.Y;
+            string coordinates = x.ToString() + " " + y.ToString();
+            float strokeWidth = circle.StrokeWidth;
+            float radius = circle.Radius;
+            JObject json = new JObject(new JProperty("type", "CIRCLE"),
+                                      new JProperty("content", new JObject(
+                                          new JProperty("colorHash", colourHash),
+                                          new JProperty("coordinates", coordinates),
+                                           new JProperty("radius", radius),
+                                           new JProperty("strokeWidth", strokeWidth))));
+            SendData(json);
+        }
+
+        public void Send(ColoredPath path)
+        {
+            string colourHash = path.Color.ToString();
+            string SVGPath = path.Path.ToSvgPathData();
+            float strokeWidth = path.StrokeWidth;
+            JObject json = new JObject(new JProperty("type", "PATH"),
+                                       new JProperty("content", new JObject(
+                                          new JProperty("svgpath", SVGPath),
+                                          new JProperty("colorHash", colourHash),
+                                          new JProperty("strokeWidth", strokeWidth))));
+            SendData(json);
+        }
+    }
+}
